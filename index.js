a8039b898a31522fe015a409edea00412d5e9c77
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2023-09-08 11:09:52 +0300

    chore: add test server

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (_req, res) => {
+    res.status(200).json({ data: [], errors: [], statusCode: 200, message: 'Hola' })
+})
+
+app.listen(9000, () => console.log('Server is running at http://localhost:9000'))
